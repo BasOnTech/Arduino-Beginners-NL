051f4cd8ef88eb9eff1c19c76194198a48ea31b4
BasOnTech <info@basontech.com>
2018-04-02 22:47:06 +0200

    RFID lezer toegevoegd

--- RFID-RC522/RFID-RC5222-deel-1.ino
+++ RFID-RC522/RFID-RC5222-deel-1.ino
@@ -1,8 +1,18 @@
 /*
- * Bas on Tech - RFID kaarten lezen
- * https://arduino-lessen.nl
+ * Bas on Tech - RFID lezer
+ * Deze les is onderdeel van de lessen op https://arduino-lessen.nl
+ *  
+ * (c) Copyright 2018 - Bas van Dijk / Bas on Tech
+ * Deze code en inhoud van de lessen mag zonder schriftelijke toestemming 
+ * niet voor commerciele doeleinden worden gebruikt
  * 
+ * YouTube:    https://www.youtube.com/c/BasOnTechNL
+ * Facebook:   https://www.facebook.com/BasOnTechChannel
+ * Instagram:  https://www.instagram.com/BasOnTech
+ * Twitter:    https://twitter.com/BasOnTech
  * 
+ * ---------------------------------------------------------------------------
+ *  * 
  * LET OP: De beveiliging van deze kaarten is zwak. Niet gebruiken voor beveiliging. De
  * kaartnummers hoeven niet perse uniek te zijn. Sommige Chinese kaarten staan toe dat
  * het nummer kan worden veranderd.
