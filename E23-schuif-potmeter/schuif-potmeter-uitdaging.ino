0f0d058b93923f96df1c3c7c733839b9e75f09bc
BasOnTech <info@basontech.com>
2019-01-11 20:27:41 +0100

    Code bijgewerkt

--- E23-schuif-potmeter/schuif-potmeter-uitdaging.ino
+++ E23-schuif-potmeter/schuif-potmeter-uitdaging.ino
@@ -24,8 +24,18 @@ void setup() {
 void loop() {
   
   potmeterVal = analogRead(potmeterPin);    // Lees de analoge waarde van de Schuif potmeter
+
+  // Het totale bereik is 1024 stappen: 0 t/m 1023
+  // In de opdracht vraag ik je van 512 t/m -512 te maken
+  // Dit zijn 1025 stappen: 2 x 512 = 1024, en dan nog 1 stap voor de 0
+  // Daarom moeten we controleren of het getal groter is dan 0
+  // Als dit zo is dan tellen we er 1 bij op om zo op 512 uit te komen
+  // in plaats van 511
+  if (potmeterVal > 0) {
+    potmeterVal++;
+  }
   
-  Serial.println(potmeterVal);        // Toon de waarde in de seriële monitor
+  Serial.println(potmeterVal - 512);        // Toon de waarde in de seriële monitor
 
   delay(100);                               // Pauzeer 100ms
   
