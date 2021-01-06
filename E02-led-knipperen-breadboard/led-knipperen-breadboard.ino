86788f2fef7cc5c4024db591ad7ae004f8b1af1f
Bas van Dijk <basvdijk@users.noreply.github.com>
2021-01-06 19:40:47 +0100

    Update led-knipperen-breadboard.ino

--- E02-led-knipperen-breadboard/led-knipperen-breadboard.ino
+++ E02-led-knipperen-breadboard/led-knipperen-breadboard.ino
@@ -21,9 +21,9 @@ void setup() {
 
 // De loop functie wordt oneindig herhaald
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // Zet de ingebouwde LED aan
-  delay(1000);                       // Pauzeer 1 seconde (1000 milliseconden)
+  digitalWrite(11, HIGH);   // Zet de ingebouwde LED aan
+  delay(1000);              // Pauzeer 1 seconde (1000 milliseconden)
   
-  digitalWrite(LED_BUILTIN, LOW);    // Zet de ingebouwde LED uit
-  delay(1000);                       // Pauzeer 1 seconde (1000 milliseconden)
+  digitalWrite(11, LOW);    // Zet de ingebouwde LED uit
+  delay(1000);              // Pauzeer 1 seconde (1000 milliseconden)
 }
